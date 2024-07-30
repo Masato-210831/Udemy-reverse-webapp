ef7e42334be75ae2cc48ebd981ebda3dc34e8fe6
masato-210831 <b86500b@gmail.com>
2024-07-30 21:56:59 +0900

    プレゼンテーション層とアプリケーション層の分離：サービスクラスの作成

--- src/presentation/gameRouter.ts
+++ src/presentation/gameRouter.ts
@@ -1,43 +1,11 @@
-import express from 'express'
-import { GameGateway } from '../dataaccess/gameGateway';
-import { TurnGateway } from '../dataaccess/turnGateway';
-import { SquareGateway } from '../dataaccess/squareGateway';
-import { connectMySQL } from '../dataaccess/connection';
-import { INITIAL_BOARD, DARK } from '../application/constants';
+import express from "express";
+import { GameService } from "../application/gameService";
 
-export const gameRouter = express.Router()
+export const gameRouter = express.Router();
 
-const gameGateway = new GameGateway();
-const turnGateway = new TurnGateway();
-const squareGateway = new SquareGateway();
-
+const gameService = new GameService();
 
 gameRouter.post("/api/games", async (req, res) => {
-  const now = new Date();
-
-  // mysqlとの連携
-  const conn = await connectMySQL();
-
-  try {
-    await conn.beginTransaction();
-
-    // gameテーブルの初期化
-    const gameRecord = await gameGateway.insert(conn, now);
-
-    // turnsテーブルの初期化
-    const turnRecord = await turnGateway.insert(
-      conn,
-      gameRecord.id,
-      0,
-      DARK,
-      now
-    );
-
-    await squareGateway.insertAll(conn, turnRecord.id, INITIAL_BOARD);
-
-    await conn.commit();
-  } finally {
-    await conn.end();
-  }
+  await gameService.startNewGame();
   res.status(201).end();
 });
