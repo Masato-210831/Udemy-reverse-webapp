c95e95cfee674e2b70eb61ded874bb8c06d7bab5
masato-210831 <b86500b@gmail.com>
2024-07-31 14:09:09 +0900

    gameService.tsのリファクタリング

--- src/application/gameService.ts
+++ src/application/gameService.ts
@@ -1,42 +1,33 @@
-import { GameGateway } from '../dataaccess/gameGateway';
-import { TurnGateway } from '../dataaccess/turnGateway';
-import { SquareGateway } from '../dataaccess/squareGateway';
-import { connectMySQL } from '../dataaccess/connection';
-import { INITIAL_BOARD, DARK } from '../application/constants';
-
+import { GameGateway } from "../dataaccess/gameGateway";
+import { connectMySQL } from "../dataaccess/connection";
+import { TurnRepository } from "../domain/turnRepository";
+import { firstTurn} from "../domain/turn";
 
 const gameGateway = new GameGateway();
-const turnGateway = new TurnGateway();
-const squareGateway = new SquareGateway();
+
+const turnRepository = new TurnRepository();
 
 export class GameService {
   async startNewGame() {
     const now = new Date();
 
-  // mysqlとの連携
-  const conn = await connectMySQL();
+    // mysqlとの連携
+    const conn = await connectMySQL();
 
-  try {
-    await conn.beginTransaction();
+    try {
+      await conn.beginTransaction();
 
-    // gameテーブルの初期化
-    const gameRecord = await gameGateway.insert(conn, now);
+      // gameテーブルの初期化
+      const gameRecord = await gameGateway.insert(conn, now);
 
-    // turnsテーブルの初期化
-    const turnRecord = await turnGateway.insert(
-      conn,
-      gameRecord.id,
-      0,
-      DARK,
-      now
-    );
+      // turnsテーブルの初期化
+      const turn = firstTurn(gameRecord.id, now)
 
-    await squareGateway.insertAll(conn, turnRecord.id, INITIAL_BOARD);
+      await turnRepository.save(conn, turn);
 
-    await conn.commit();
-  } finally {
-    await conn.end();
-  }
-  
+      await conn.commit();
+    } finally {
+      await conn.end();
+    }
   }
 }
--- src/domain/turn.ts
+++ src/domain/turn.ts
@@ -1,4 +1,4 @@
-import { Board } from "./board";
+import { Board, initialBoard } from "./board";
 import { Disc } from "./disc";
 import { Move } from "./move";
 import { Point } from "./point";
@@ -63,4 +63,15 @@ export class Turn {
     return this._move
   }
 
+}
+
+export function firstTurn(gameId: number, endAt: Date) {
+  return new Turn(
+    gameId,
+    0,
+    Disc.Dark,
+    undefined,
+    initialBoard,
+    endAt
+  )
 }
