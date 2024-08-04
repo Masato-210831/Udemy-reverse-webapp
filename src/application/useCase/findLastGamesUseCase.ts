e7a9f873425b1d599d6b4ebf233c5e0f482a8246
masato-210831 <b86500b@gmail.com>
2024-08-04 23:44:36 +0900

    finished udemy lecture

---
+++ src/application/useCase/findLastGamesUseCase.ts
@@ -0,0 +1,18 @@
+import { FindLastGamesQueryService } from "../query/findLastGamesQueryService";
+import { FindLastGamesQueryModel } from "../../application/query/findLastGamesQueryService"
+import { connectMySQL } from "../../infrastructure/connection";
+
+
+const FIND_COUNT  = 10
+
+export class FindLastGamesUseCase {
+  constructor(private _queryService: FindLastGamesQueryService) {}
+  async run(): Promise<FindLastGamesQueryModel[]> {
+    const conn = await connectMySQL()
+    try {
+      return await this._queryService.query(conn, FIND_COUNT)
+    } finally {
+      conn.end()
+    }
+  }
+}
