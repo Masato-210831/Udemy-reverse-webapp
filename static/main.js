fce6360073641451bd31d69cf06ad46166369838
masato-210831 <b86500b@gmail.com>
2024-08-04 10:51:44 +0900

    waringパターンを実装

--- static/main.js
+++ static/main.js
@@ -2,14 +2,23 @@ const EMPTY = 0;
 const DARK = 1;
 const LIGHT = 2;
 
+const WINNER_DRAW = 0
+const WINNER_DARK = 1
+const WINNER_LIGHT = 2
+
+
 const boardElement = document.getElementById("board");
 const nextDiscMessageElement = document.getElementById("next-disc-message")
+const warningMessageElement = document.getElementById('warning-message')
 
-async function showBoard(turnCount) {
+async function showBoard(turnCount, previousDisc ) {
   const response = await fetch(`/api/games/latest/turns/${turnCount}`);
   const responseBody = await response.json();
   const board = responseBody.board;
   const nextDisc = responseBody.nextDisc
+  const winnerDisc = responseBody.winnerDisc
+
+  showWarningMessage(previousDisc, nextDisc, winnerDisc)
 
   showNextDiscMessage(nextDisc)
 
@@ -36,7 +45,7 @@ async function showBoard(turnCount) {
           const nextTurnCount = turnCount + 1
           const registerTurnResponse = await registerTurn(nextTurnCount, nextDisc, x, y)
           if (registerTurnResponse.ok) {
-            await showBoard(nextTurnCount)
+            await showBoard(nextTurnCount, nextDisc)
           }
         })
       }
@@ -46,10 +55,49 @@ async function showBoard(turnCount) {
   });
 }
 
+function discToString(Disc) {
+  return Disc === DARK ? '黒' : '白'
+}
+
+function showWarningMessage(previousDisc, nextDisc, winnerDisc) {
+
+  // 処理が複雑になるので、同じ引数を受け取る関数を挟む
+  const message = warningMessage(previousDisc, nextDisc, winnerDisc)
+
+  warningMessageElement.innerText = message
+
+  if (message === null) {
+    warningMessageElement.style.display = 'none'
+  } else {
+    warningMessageElement.style.display = 'block'
+  }
+}
+
+function warningMessage(previousDisc, nextDisc, winnerDisc) {
+
+  // スキップされた時の分岐
+  // 勝敗を決した際の分岐
+  if (nextDisc !== null) {
+    if (previousDisc === nextDisc) {
+
+      // スキップされたディスク
+      const skipped = nextDisc === DARK ? LIGHT : DARK
+      return `${discToString(skipped)}の番はスキップです`
+    } else {
+      return null
+    }
+  } else {
+    if (winnerDisc === WINNER_DRAW) {
+      return '引き分けです'
+    } else {
+      return `${discToString(winnerDisc)}の勝ちです`
+    } 
+  }
+}
+
 function showNextDiscMessage(nextDisc) {
   if(nextDisc) {
-    const color = nextDisc === DARK ? '黒' : '白'
-    nextDiscMessageElement.innerText = `次は${color}の番です。`
+    nextDiscMessageElement.innerText = `次は${discToString(nextDisc)}の番です。`
   } else {
     nextDiscMessageElement.innerText = ''
   }
